433a3d7e1868195594ad729615b778de953d79f4
andytorrestb <andytorrestb@gmail.com>
2018-07-30 23:03:52 -0400

    idek

--- index.php
+++ index.php
@@ -5,17 +5,21 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
+// Create and configure Slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config);
+
 $app->get('/', function (Request $request, Response $response, array $args) {
 	$content = require_once __DIR__.'/home.php';
     $response->write($content);
     return $response;
 });
 
-$app->get('/diy', function (Request $request, Response $response, array $args) {
-	$content = require_once __DIR__.'/diy.php';
-    $response->write($content);
-    return $response;
+$app->get('/docs', function ($request, $response, $args) {
+	$content = require_once(__DIR__.'/docs.php');
+    return $response->write($content);
 });
 
 
