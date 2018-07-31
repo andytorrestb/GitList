c8dd87c42a9cc1e75444ba3526538cbe66e265d9
andytorrestb <andytorrestb@gmail.com>
2018-07-30 22:22:58 -0400

    added route to home.php WIP

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '/Gitlist/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/', function (Request $request, Response $response, array $args) {
 	$content = require_once __DIR__.'/home.php';
-    $response->getBody()->write($content);
+    $response->write($content);
     return $response;
 });
 
